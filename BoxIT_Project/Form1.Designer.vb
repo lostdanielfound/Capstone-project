b3008fee6b0731ceccf68358ed704e4786b56a95
Daniel Guzman <danielsonguzman2000@gmail.com>
2023-09-14 20:40:06 -0700

    Basic Backup feature

---
+++ BoxIT_Project/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        AddBackupPlanBtn = New Button()
+        Label1 = New Label()
+        Panel1 = New Panel()
+        Label2 = New Label()
+        AddDestBtn = New Button()
+        BackupBtn = New Button()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' AddBackupPlanBtn
+        ' 
+        AddBackupPlanBtn.Location = New Point(3, 3)
+        AddBackupPlanBtn.Name = "AddBackupPlanBtn"
+        AddBackupPlanBtn.Size = New Size(139, 23)
+        AddBackupPlanBtn.TabIndex = 0
+        AddBackupPlanBtn.Text = "+ Add Backup Plan"
+        AddBackupPlanBtn.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(148, 7)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(172, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Please add plan to show source"
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(BackupBtn)
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(AddDestBtn)
+        Panel1.Controls.Add(AddBackupPlanBtn)
+        Panel1.Controls.Add(Label1)
+        Panel1.Location = New Point(12, 12)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(776, 209)
+        Panel1.TabIndex = 2
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(148, 36)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(114, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "Default Destination: "
+        ' 
+        ' AddDestBtn
+        ' 
+        AddDestBtn.Enabled = False
+        AddDestBtn.Location = New Point(3, 32)
+        AddDestBtn.Name = "AddDestBtn"
+        AddDestBtn.Size = New Size(139, 23)
+        AddDestBtn.TabIndex = 2
+        AddDestBtn.Text = "+ Add Destination"
+        AddDestBtn.UseVisualStyleBackColor = True
+        ' 
+        ' BackupBtn
+        ' 
+        BackupBtn.Enabled = False
+        BackupBtn.Location = New Point(3, 183)
+        BackupBtn.Name = "BackupBtn"
+        BackupBtn.Size = New Size(139, 23)
+        BackupBtn.TabIndex = 4
+        BackupBtn.Text = "Start Backup"
+        BackupBtn.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Panel1)
+        Name = "Form1"
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents AddBackupPlanBtn As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents AddDestBtn As Button
+    Friend WithEvents BackupBtn As Button
+End Class
---
+++ BoxIT_Project/Form1.vb
@@ -0,0 +1,92 @@
+﻿Imports System.IO.Compression
+
+Public Class Form1
+
+    '===================
+    ' Auxiliary Methods
+    '===================
+    Private Sub Log(LogMessage As String)
+        My.Application.Log.WriteEntry("=== " & LogMessage & " ===")
+    End Sub
+
+    Private Sub ReadDirectory(Directory As String)
+        Log("Files read from directory: " & Directory)
+
+        Dim Idx As Integer = 0
+        For Each file As String In My.Computer.FileSystem.GetFiles(Directory)
+            Log("[" & Idx & "]" & " " & file)
+            Idx += 1
+        Next
+
+        For Each folder As String In My.Computer.FileSystem.GetDirectories(Directory)
+            Log("[" & Idx & "]" & " " & folder)
+            Idx += 1
+        Next
+
+    End Sub
+
+    '===================
+    ' Form variables
+    '===================
+    Dim Src As String
+    Dim Dest As String = "%userprofile\Documents\BoxIT_Backups" 'Default Backup destination
+    Dim SrcChosen As Boolean = False
+    Dim DestChosen As Boolean = False
+    Dim DefaultSelection As Boolean = True
+
+    '===================
+    ' Event Handlers
+    '===================
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles AddBackupPlanBtn.Click
+        Dim folderSelectionDig As New FolderBrowserDialog
+        folderSelectionDig.ShowNewFolderButton = False
+
+        Log("Showing folder selection Dialog to select backup src")
+        Dim ReturnResult As DialogResult = folderSelectionDig.ShowDialog()
+
+        Select Case ReturnResult
+            Case DialogResult.OK 'Set the backup source
+                Src = folderSelectionDig.SelectedPath
+                Label1.Text = folderSelectionDig.SelectedPath
+                SrcChosen = True
+                Log("Set Src as " & Src)
+
+                AddDestBtn.Enabled = True
+            Case DialogResult.Cancel 'Dialog failed, disable AddDestBtn if Src is empty
+                AddDestBtn.Enabled = (Src <> "")
+        End Select
+    End Sub
+
+    Private Sub AddDestBtnClick(sender As Object, e As EventArgs) Handles AddDestBtn.Click
+        Dim folderSelectionDig As New FolderBrowserDialog
+        folderSelectionDig.ShowNewFolderButton = False
+
+        If folderSelectionDig.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            Dest = folderSelectionDig.SelectedPath
+            Label2.Text = Dest
+            DestChosen = True
+
+            Log("Set Dest as " & Dest)
+
+            'Unlock Start Backup Button after user has selected Src and Dest
+            If SrcChosen And DestChosen Then
+                BackupBtn.Enabled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub BackupBtn_Click(sender As Object, e As EventArgs) Handles BackupBtn.Click
+        '1st Obj: Perform token reading,
+        'Check to ensure that any folder / file is named the same as output Zip file
+        ReadDirectory(Dest)
+
+        '2nd Obj: Create Zip file and store it within Dest directory
+        Log("Compressing Src directory and storing it in " & Dest)
+        ZipFile.CreateFromDirectory(Src, Dest & "\Backup.zip", CompressionLevel.SmallestSize, False)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label2.Text = ""
+    End Sub
+End Class
